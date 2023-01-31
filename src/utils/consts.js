fee3d84d782856a3a3e170d530f3c79dd4023fe7
Vova <uladzimirfilipau@gmail.com>
2023-01-31 12:39:58 +0300

    feat: App

---
+++ src/utils/consts.js
@@ -0,0 +1,35 @@
+export const mainTitle = 'Ты сегодня покормил кота?';
+
+export const basicData = {
+  articleText: 'Сказочное заморское яство',
+  articleTextHover: 'Котэ не одобряет?',
+  articleTitle: 'Нямушка',
+  weightSpan: 'кг',
+  itemText: 'Чего сидишь? Порадуй котэ, ',
+  itemButtonText: 'купи',
+  itemTextSpan: '.',
+};
+
+export const optionsData = [
+  {
+    taste: 'с фуа-гра',
+    weight: '0,5',
+    description: ['10 порций', 'мышь в подарок'],
+    textSelected: 'Печень утки разварная с артишоками.',
+    textDisabled: 'Печалька, с фуа-гра закончился.',
+  },
+  {
+    taste: 'с рыбой',
+    weight: '2',
+    description: ['40 порций', '2 мыши в подарок'],
+    textSelected: 'Головы щучьи с чесноком да свежайшая сёмгушка.',
+    textDisabled: 'Печалька, с рыбой закончился.',
+  },
+  {
+    taste: 'с курой',
+    weight: '5',
+    description: ['100 порций', '5 мышей в подарок', 'заказчик доволен'],
+    textSelected: 'Филе из цыплят с трюфелями в бульоне.',
+    textDisabled: 'Печалька, с курой закончился.',
+  },
+];
